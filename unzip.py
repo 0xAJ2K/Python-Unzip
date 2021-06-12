888f7bd76ca0b663c528b48a1e23d5d340df7d9c
AJ2K <48768952+0xAJ2K@users.noreply.github.com>
2021-06-12 17:48:03 +0100

    Add files via upload

---
+++ unzip.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys, argparse
+from zipfile import PyZipFile
+
+def error():
+     parser.print_help()
+     quit()
+
+parser = argparse.ArgumentParser(description='Args')
+requiredNamed = parser.add_argument_group('Required Arguments')
+parser.add_argument("-f", dest="file", help="The ZIP file to unzip.")
+args = parser.parse_args()
+
+try:
+	pzf = PyZipFile(args.file)
+	pzf.extractall()
+except:
+	error()
